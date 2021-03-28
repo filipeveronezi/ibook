b605841934f02e193c69f9a0a0747b82419f7206
Filipe Veronezi <filipeseidi@hotmail.com>
2021-03-28 17:05:11 -0300

    Nuxt

---
+++ components/molecules/BookCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink class="book-card"></NuxtLink>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped></style>
--- components/organisms/BookList.vue
+++ components/organisms/BookList.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="book-lis">
+  <div class="book-list">
     <h2>Listagem de livros</h2>
+
+    <BookCard v-for="book in $books" :key="book.id" :book="book" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { books } from '@/store'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    $books() {
+      return books.$all
+    }
+  }
+})
 </script>
 
 <style scoped></style>
